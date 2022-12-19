4f31ebed50fcfe33df86dbbe0ad77ff67ff3c527
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-19 19:16:44 +0800

    update fix on url category taxonomy

---
+++ blog-frontend/components/card.js
@@ -0,0 +1,17 @@
+import { Card } from '@mui/material';
+import React, {Fragment} from 'react';
+
+function Card() {
+    
+    const setCard = () => {
+
+    }
+    
+    return(
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export {Card};
